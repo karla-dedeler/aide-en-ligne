82539067b26ca52d0e757fb03153a3d976b1933b
karla dedeler <karla.dedeler@gestimum.com>
2023-06-14 10:21:29 +0200

    maj html option

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'collapse_navigation': False,
+    'navigation_depth': 2,
     'prev_next_buttons_location': 'None',
     'titles_only' : True
 }
