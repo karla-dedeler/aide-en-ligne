8af97504a7aae086df633bc438e9e8c8e7b8af30
karla dedeler <karla.dedeler@gestimum.com>
2023-06-14 10:31:11 +0200

    maj theme

--- docs/conf.py
+++ docs/conf.py
@@ -30,12 +30,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'collapse_navigation': False,
-    'navigation_depth': 2,
-    'prev_next_buttons_location': 'None',
-    'titles_only' : True
-}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
